34875e4655267da053a6b9956b06bf35a0a2d7c6
qnrjs42 <qnrjs42@naver.com>
2023-01-01 17:11:32 +0900

    #4.1 setState

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -12,11 +12,12 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  int counter = 0;
+  List<int> numbers = [];
 
   void onClicked() {
     setState(() {
-      counter++;
+      numbers.add(numbers.length);
+      print(numbers);
     });
   }
 
@@ -32,12 +33,7 @@ class _AppState extends State<App> {
                   style: TextStyle(
                     fontSize: 30,
                   )),
-              Text(
-                '$counter',
-                style: const TextStyle(
-                  fontSize: 30,
-                ),
-              ),
+              for (var n in numbers) Text('$n'),
               IconButton(
                 onPressed: onClicked,
                 icon: const Icon(Icons.add_box_rounded),
