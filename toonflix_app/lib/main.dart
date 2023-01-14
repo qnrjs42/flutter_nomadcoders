83c39293c082bdf2783bee819fa13ccc369cad5a
qnrjs42 <qnrjs42@naver.com>
2023-01-14 18:05:58 +0900

    #6.2 Data Fetching

--- toonflix_app/lib/main.dart
+++ toonflix_app/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:toonflix_app/screens/home_screen.dart';
+import 'package:toonflix_app/services/api_service.dart';
 
 void main() {
+  ApiService().getTodaysToons();
   runApp(const MyApp());
 }
 
