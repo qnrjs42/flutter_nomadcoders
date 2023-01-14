5d9543d815843f6228f8bf6917770bbd3c27585d
qnrjs42 <qnrjs42@naver.com>
2023-01-14 10:25:38 +0900

    #5.2. Pause Play

--- pomodoro/lib/screens/home_screen.dart
+++ pomodoro/lib/screens/home_screen.dart
@@ -11,6 +11,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int totalSeconds = 1500;
+  bool isRunning = false;
   late Timer timer;
 
   void onTick(Timer timer) {
@@ -21,6 +22,18 @@ class _HomeScreenState extends State<HomeScreen> {
 
   void onStartPressed() {
     timer = Timer.periodic(const Duration(seconds: 1), onTick);
+
+    setState(() {
+      isRunning = true;
+    });
+  }
+
+  void onPausePressed() {
+    timer.cancel();
+
+    setState(() {
+      isRunning = false;
+    });
   }
 
   @override
@@ -49,8 +62,10 @@ class _HomeScreenState extends State<HomeScreen> {
                 child: IconButton(
               iconSize: 120,
               color: Theme.of(context).cardColor,
-              onPressed: onStartPressed,
-              icon: const Icon(Icons.play_circle_outline),
+              onPressed: isRunning ? onPausePressed : onStartPressed,
+              icon: Icon(isRunning
+                  ? Icons.pause_circle_outline
+                  : Icons.play_circle_outline),
             )),
           ),
           Flexible(
